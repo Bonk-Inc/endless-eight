0eda2da9ba273d4cd86c0b823fa07b0f047614d3
Joost <joost.valentijn2002@gmail.com>
2020-10-03 13:14:43 +0200

    added movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField] private PlayerController controller;
+    private bool left;
+    private bool right;
+    private bool up;
+    private bool down;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) up = true;
+        else if (Input.GetKey(KeyCode.S)) down = true;
+
+        if (Input.GetKey(KeyCode.A)) right = true;
+        else if (Input.GetKey(KeyCode.D)) left = true;
+
+
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(left, right, up, down);
+        up = false;
+        down = false;
+        right = false;
+        left = false;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] [Range(0, 500)] private float speedUpDown;
+    [SerializeField] [Range(0, 500)] private float speedLeftRight;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private Transform lowestPointOfGameObject;
+
+    private bool isGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(lowestPointOfGameObject.position, 0.3f, ground);
+
+        foreach (Collider2D coll in colliders)
+        {
+            if (coll.gameObject != gameObject)
+            {
+                isGrounded = true;
+            }
+            
+        }
+    }
+
+    public void Move(bool left, bool right, bool up, bool down)
+    {
+        Vector3 velocity = new Vector2();
+
+        if (left) velocity = new Vector3(speedLeftRight,0,0);
+        if (right) velocity += new Vector3(-speedLeftRight, 0,0);
+        if (up) velocity += new Vector3(0,0, speedUpDown);
+        if (down) velocity += new Vector3(0,0, -speedUpDown);
+
+        gameObject.GetComponent<Rigidbody>().velocity = velocity;
+
+    }
+}
