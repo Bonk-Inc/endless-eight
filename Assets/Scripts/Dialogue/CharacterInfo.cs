3f5c5409c94ce61b1cb67c378e1e3ed505ac1cc3
jvdwijk <joeyvdwijk@gmail.com>
2020-10-03 13:03:29 +0200

    characterInfo

---
+++ Assets/Scripts/Dialogue/CharacterInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfo : MonoBehaviour
+{
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private Sprite portrait;
+
+    public string Name => name;
+    public Sprite Portrait => portrait;
+
+}
