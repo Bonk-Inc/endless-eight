fb075780a5762058d2a5cfda0df453ce9bfb6b21
Joost <joost.valentijn2002@gmail.com>
2020-10-03 16:47:39 +0200

    added loopmanager, a timer and a conversationplayer class. modified movement so it is 2d

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    private float time;
+    [SerializeField] private float maxTime;
+    [SerializeField]private TextMeshProUGUI pro;
+    private float timeRemaining;
+    [SerializeField] private UnityEvent outOfTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void FixedUpdate()
+    {
+        timeRemaining = (maxTime - time);
+        
+        pro.SetText(Math.Floor(timeRemaining).ToString());
+        time += Time.deltaTime;
+        if (timeRemaining <= 0)
+        {
+            time = 0;
+            outOfTime?.Invoke();
+            
+        }
+    }
+}
