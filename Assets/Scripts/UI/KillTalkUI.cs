61aa816f977b162b1466d2b38ffdcfa34f476162
Coen Steuns <c.steuns@gmail.com>
2020-10-05 09:09:56 +0200

    Interaction UI

---
+++ Assets/Scripts/UI/KillTalkUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillTalkUI : MonoBehaviour
+{
+    [SerializeField]
+    private Button killButton, talkButton;
+
+    [SerializeField]
+    private Image portraitUI;
+
+    public void Activate(Sprite portrait, Action OnKill, Action OnTalk)
+    {
+        gameObject.SetActive(true);
+
+        portraitUI.sprite = portrait;
+
+        killButton.onClick.RemoveAllListeners();
+        killButton.onClick.AddListener(() => OnKill.Invoke());
+
+        talkButton.onClick.RemoveAllListeners();
+        talkButton.onClick.AddListener(() => OnTalk.Invoke());
+    }
+
+    public void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
