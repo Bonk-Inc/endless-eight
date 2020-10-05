5c38cbf0a3728308f18fd57500b4ea2fd15ac152
Mubble <noahtekent@gmail.com>
2020-10-05 22:59:31 +0200

    Start Menu Updated w/ Exta Buttons

---
+++ Assets/credits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class credits : MonoBehaviour
+{
+
+    [SerializeField] private GameObject gObject;
+    public void showCredits()
+    {
+        gObject.SetActive(true);
+    }
+
+    public void hideCredits()
+    {
+        gObject.SetActive(false);
+    }
+}
