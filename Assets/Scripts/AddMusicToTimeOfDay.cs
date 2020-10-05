ce61c7bb9718b16c3f38247a55496bc95a13b0a6
Joost <joost.valentijn2002@gmail.com>
2020-10-05 23:15:52 +0200

    added sound when entering the bar

--- Assets/Scripts/AddMusicToTimeOfDay.cs
+++ Assets/Scripts/AddMusicToTimeOfDay.cs
@@ -7,30 +7,38 @@ public class AddMusicToTimeOfDay : TimeblockActionManager
     [SerializeField] private AudioSource morningMusic;
     [SerializeField] private AudioSource afternoonMusic;
     [SerializeField] private AudioSource eveningMusic;
+    public AudioSource currentlyplaying;
+    public bool musicFromRoom = false;
     protected override void HandleTimeblockChange(TimeBlockType newTimeblock)
     {
         switch (newTimeblock)
         {
             case TimeBlockType.NONE:
-                
+
                 break;
             case TimeBlockType.MORNING:
                 BeginNewSound(morningMusic);
+                currentlyplaying = morningMusic;
                 break;
             case TimeBlockType.AFTERNOON:
                 BeginNewSound(afternoonMusic);
+                currentlyplaying = afternoonMusic;
                 break;
             case TimeBlockType.EVENING:
                 BeginNewSound(eveningMusic);
+                currentlyplaying = eveningMusic;
                 break;
             default:
                 break;
         }
 
-
-
-
+        if (musicFromRoom)
+        {
+            currentlyplaying.volume = 0;
+        }
     }
+
+    
 
     private void BeginNewSound(AudioSource current)
     {
