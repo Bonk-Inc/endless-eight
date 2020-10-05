8be3a4138f13b94fa0918136aa4f510a2d05938b
Joost <joost.valentijn2002@gmail.com>
2020-10-05 17:51:40 +0200

    added music to game

---
+++ Assets/Scripts/AddMusicToTimeOfDay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMusicToTimeOfDay : TimeblockActionManager
+{
+    [SerializeField] private AudioSource morningMusic;
+    [SerializeField] private AudioSource afternoonMusic;
+    [SerializeField] private AudioSource eveningMusic;
+    protected override void HandleTimeblockChange(TimeBlockType newTimeblock)
+    {
+        switch (newTimeblock)
+        {
+            case TimeBlockType.NONE:
+                
+                break;
+            case TimeBlockType.MORNING:
+                BeginNewSound(morningMusic);
+                break;
+            case TimeBlockType.AFTERNOON:
+                BeginNewSound(afternoonMusic);
+                break;
+            case TimeBlockType.EVENING:
+                BeginNewSound(eveningMusic);
+                break;
+            default:
+                break;
+        }
+
+
+
+
+    }
+
+    private void BeginNewSound(AudioSource current)
+    {
+        morningMusic.Stop();
+        afternoonMusic.Stop();
+        eveningMusic.Stop();
+        current.Play();
+    }
+}
