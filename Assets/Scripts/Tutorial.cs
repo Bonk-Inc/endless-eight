1c21d798647d96e3c84601967ce8d3ad996a80ce
Mubble <noahtekent@gmail.com>
2020-10-05 16:24:26 +0200

    Tutorial implementation

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] private DialogueTreePlayer treePlayer;
+    [SerializeField] private DialogueTree dialogueTree;
+
+    void Start()
+    {
+        Debug.Log("k");
+        treePlayer.PlayTree(dialogueTree);
+    }
+
+
+    void Update()
+    {
+        if(!treePlayer.IsInDialogue)
+        {
+            SceneManager.LoadScene("play");
+        }
+    }
+}
