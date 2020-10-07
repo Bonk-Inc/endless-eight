cb931d147376b30c6ef3400c283273afc70158fc
jvdwijk <joeyvdwijk@gmail.com>
2020-10-07 23:29:22 +0200

    hint ui

---
+++ Assets/Scripts/UI/HintUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HintUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI hintText;
+    private void Start()
+    {
+        PossibleTarget target = TargetManagement.Instance.CurrentTarget;
+        hintText.text = target.TargetHint.Lines[1].Line;
+    }
+
+}
